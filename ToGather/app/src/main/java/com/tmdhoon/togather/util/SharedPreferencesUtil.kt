764eb10db9f44716fc7d165d861adb1b30457888
Tmdhoon2 <hks026naver@gmail.com>
2022-10-13 14:21:36 +0900

    ♻️ refactor :: refactoring SharedPreferencesUtil

--- ToGather/app/src/main/java/com/tmdhoon/togather/util/SharedPreferencesUtil.kt
+++ ToGather/app/src/main/java/com/tmdhoon/togather/util/SharedPreferencesUtil.kt
@@ -1,36 +1,25 @@
 package com.tmdhoon.togather.util
 
+import android.content.Context
 import android.content.SharedPreferences
-import android.icu.number.IntegerWidth
-import androidx.appcompat.app.AppCompatActivity
 
-object SharedPreferencesUtil : AppCompatActivity() {
-    private val sharedPreferences : SharedPreferences by lazy {
-        getSharedPreferences("User", MODE_PRIVATE)
+fun initPref(context: Context, mode: Int): SharedPreferences {
+    return context.getSharedPreferences("user", mode)
+}
+
+
+fun putPref(editor: SharedPreferences.Editor, key: String, value: Any?) {
+    when(value){
+        is String -> editor.putString(key, value).apply()
+        is Int -> editor.putInt(key, value).apply()
+        is Boolean -> editor.putBoolean(key, value).apply()
     }
+}
 
-    private val editor : SharedPreferences.Editor by lazy {
-        sharedPreferences.edit()
+fun getPref(preferences: SharedPreferences, key: String, value: Any?) : Any?{
+    return when(value){
+        is String -> preferences.getString(key, value)
+        is Int -> preferences.getInt(key, value)
+        else -> preferences.getBoolean(key, value as Boolean)
     }
-
-    private lateinit var result : String
-
-    fun <T> put(key : String, value : T){
-        when(value){
-            (value is String) -> editor.putString(key, value.toString()).apply()
-            (value is Int) -> editor.putInt(key, Integer.parseInt(value.toString())).apply()
-            (value is Boolean) -> editor.putBoolean(key, value as Boolean)
-        }
-    }
-
-    fun <T> get(key : String, value : T) : String {
-
-        when(value){
-            (value is String) -> result = sharedPreferences.getString(key, value.toString()).toString()
-            (value is Int) -> result = sharedPreferences.getInt(key, Integer.parseInt(value.toString())) as String
-            (value is Boolean) -> result = sharedPreferences.getBoolean(key, value as Boolean) as String
-        }
-        return result
-    }
-
 }
