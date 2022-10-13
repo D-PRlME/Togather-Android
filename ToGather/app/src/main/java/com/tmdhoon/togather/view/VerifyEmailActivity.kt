ac60c62313161f4d2153112376d2f8011059e476
Tmdhoon2 <hks026naver@gmail.com>
2022-10-13 14:22:28 +0900

    ♻️ refactor :: preferences instance initializing

--- ToGather/app/src/main/java/com/tmdhoon/togather/view/VerifyEmailActivity.kt
+++ ToGather/app/src/main/java/com/tmdhoon/togather/view/VerifyEmailActivity.kt
@@ -1,17 +1,15 @@
 package com.tmdhoon.togather.view
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.tmdhoon.togather.R
 import com.tmdhoon.togather.databinding.ActivityVerifyEmailBinding
-import com.tmdhoon.togather.util.ACCESS_TOKEN
-import com.tmdhoon.togather.util.IntentUtil
-import com.tmdhoon.togather.util.SharedPreferencesUtil
-import com.tmdhoon.togather.util.ToastUtil
-import com.tmdhoon.togather.viewmodel.LoginViewModel
+import com.tmdhoon.togather.util.*
 import com.tmdhoon.togather.viewmodel.RegisterViewModel
 
 class VerifyEmailActivity : AppCompatActivity() {
@@ -24,8 +22,12 @@ class VerifyEmailActivity : AppCompatActivity() {
         ViewModelProvider(this).get(RegisterViewModel::class.java)
     }
 
-    private val loginViewModel : LoginViewModel by lazy {
-        ViewModelProvider(this).get(LoginViewModel::class.java)
+    private val pref : SharedPreferences by lazy {
+        initPref(this, MODE_PRIVATE)
+    }
+
+    private val editor : SharedPreferences.Editor by lazy {
+        pref.edit()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,11 +41,13 @@ class VerifyEmailActivity : AppCompatActivity() {
     private fun initNextButton() {
         val code = binding.etVerifyEmailCode.text
         binding.btVerifyEmailNext.setOnClickListener {
+            Log.d("TEST", "clicked")
             if (code.isNotBlank()) {
                 registerViewModel.verifyCode(
-                    SharedPreferencesUtil.get("email", ""),
-                    Integer.parseInt(code.toString())
+                    getPref(pref, "email", "").toString(),
+                    code.toString()
                 )
+                Log.d("TEST", getPref(pref, "email", "").toString())
             }
         }
     }
@@ -54,14 +58,13 @@ class VerifyEmailActivity : AppCompatActivity() {
                 204 -> {
                     ToastUtil.print(this, "이메일이 성공적으로 인증되었습니다!")
                     registerViewModel.register(
-                        SharedPreferencesUtil.get("email", ""),
-                        SharedPreferencesUtil.get("pw", ""),
-                        SharedPreferencesUtil.get("name", "")
+                        getPref(pref, "email", "").toString(),
+                        getPref(pref, "pw", "").toString(),
+                        getPref(pref, "name", "").toString(),
                     )
+                    putPref(editor, "VerifyEmail", true)
                 }
-                401 -> {
-                    ToastUtil.print(this, "인증코드가 잘못되었습니다!")
-                }
+                401 -> ToastUtil.print(this, "인증코드가 잘못되었습니다!")
 
             }
         })
@@ -69,20 +72,18 @@ class VerifyEmailActivity : AppCompatActivity() {
 
     private fun initObserveRegister() {
         registerViewModel.registerResponse.observe(this, Observer {
-            when(it.code()){
-                201 ->{
+            when (it.code()) {
+                201 -> {
                     IntentUtil.startIntent(this, SuccessActivity::class.java)
                     finish()
                 }
-                400 ->{
+                400 -> {
                     ToastUtil.print(this, "항목을 확인해주세요!")
+                    IntentUtil.startIntent(this, RegisterActivity::class.java)
+                    finish()
                 }
-                401 ->{
-                    ToastUtil.print(this, "인증되지 않은 이메일 입니다")
-                }
-                409 ->{
-                    ToastUtil.print(this, "중복된 아이디 입니다")
-                }
+                401 -> ToastUtil.print(this, "인증되지 않은 이메일 입니다")
+                409 -> ToastUtil.print(this, "중복된 아이디 입니다")
             }
         })
     }
