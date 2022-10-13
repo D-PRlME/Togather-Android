4fd5e9d815da03d227eb5c2cd42e66dc4ea6a201
Tmdhoon2 <hks026naver@gmail.com>
2022-10-13 13:48:22 +0900

    ✨ feat :: SuccessActivity

---
+++ ToGather/app/src/main/java/com/tmdhoon/togather/view/SuccessActivity.kt
@@ -0,0 +1,26 @@
+package com.tmdhoon.togather.view   
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.tmdhoon.togather.R
+import com.tmdhoon.togather.databinding.ActivitySuccessBinding
+import com.tmdhoon.togather.util.IntentUtil
+
+class SuccessActivity : AppCompatActivity() {
+
+    private val binding : ActivitySuccessBinding by lazy {
+        DataBindingUtil.setContentView(this, R.layout.activity_success)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initNextButton()
+    }
+
+    private fun initNextButton() {
+        binding.btSuccessNext.setOnClickListener {
+            IntentUtil.startIntent(this, MainActivity::class.java)
+        }
+    }
+}
