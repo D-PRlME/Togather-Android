a5d3b77e954e59d24a6e97226590b178b4d30771
Tmdhoon2 <hks026naver@gmail.com>
2022-11-17 18:23:42 +0900

    ✨ feat :: implement dynamic view logic

--- ToGather/app/src/main/java/com/tmdhoon/togather/view/fragment/DetailFragment.kt
+++ ToGather/app/src/main/java/com/tmdhoon/togather/view/fragment/DetailFragment.kt
@@ -3,11 +3,19 @@ package com.tmdhoon.togather.view.fragment
 import android.app.Dialog
 import android.content.Context.MODE_PRIVATE
 import android.os.Bundle
+import android.print.PrintAttributes.Margins
+import android.util.Log
+import android.view.Display
 
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.LinearLayout
+import androidx.core.view.marginTop
+import androidx.core.view.setMargins
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
@@ -16,6 +24,8 @@ import com.tmdhoon.togather.databinding.FragmentDetailBinding
 import com.tmdhoon.togather.util.getPref
 import com.tmdhoon.togather.util.initPref
 import com.tmdhoon.togather.viewmodel.DetailViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 class DetailFragment : BottomSheetDialogFragment() {
@@ -34,6 +44,7 @@ class DetailFragment : BottomSheetDialogFragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false)
         binding.lifecycleOwner = this
         binding.detailViewModel = detailViewModel
+        observeDetailResponse()
 
         detailViewModel.getPosts(
             getPref(
@@ -54,4 +65,15 @@ class DetailFragment : BottomSheetDialogFragment() {
         return dialog
     }
 
+    private fun observeDetailResponse(){
+        detailViewModel.detailResponse.observe(viewLifecycleOwner) {
+
+            if (!it.body()!!.is_mine) {
+                binding.run {
+                    btDetailDelete.visibility = View.INVISIBLE
+                    btDetailEdit.visibility = View.INVISIBLE
+                }
+            }
+        }
+    }
 }
