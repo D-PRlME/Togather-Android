426a2ae03e500774ce2107cad0de3a78b896679b
Tmdhoon2 <hks026naver@gmail.com>
2022-11-19 22:21:48 +0900

    ✨ feat :: xml databinding

--- ToGather/app/src/main/java/com/tmdhoon/togather/view/fragment/AccountEditFragment.kt
+++ ToGather/app/src/main/java/com/tmdhoon/togather/view/fragment/AccountEditFragment.kt
@@ -2,8 +2,6 @@ package com.tmdhoon.togather.view.fragment
 
 import android.app.Dialog
 import android.os.Bundle
-import android.provider.ContactsContract.Data
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,13 +11,13 @@ import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.tmdhoon.togather.R
-import com.tmdhoon.togather.databinding.BottomSheetAccountEditBinding
+import com.tmdhoon.togather.databinding.FragmentAccountEditBinding
 import com.tmdhoon.togather.util.printToast
 import com.tmdhoon.togather.viewmodel.MyInfoViewModel
 
 class AccountEditFragment : BottomSheetDialogFragment() {
 
-    private lateinit var binding : BottomSheetAccountEditBinding
+    private lateinit var binding : FragmentAccountEditBinding
 
     private val myInfoViewModel : MyInfoViewModel by lazy {
         MyInfoViewModel()
@@ -63,7 +61,7 @@ class AccountEditFragment : BottomSheetDialogFragment() {
     }
 
     private fun initDataBinding(inflater : LayoutInflater, container: ViewGroup?){
-        binding = DataBindingUtil.inflate(inflater, R.layout.bottom_sheet_account_edit, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_account_edit, container, false)
         binding.user = myInfoViewModel
         binding.accountEditFragment = this
         binding.lifecycleOwner = this
