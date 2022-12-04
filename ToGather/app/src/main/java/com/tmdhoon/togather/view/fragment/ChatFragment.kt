c18d4f6a8d7964fe27b3ea6d696427e3eda49c78
Tmdhoon2 <hks026naver@gmail.com>
2022-12-04 11:23:19 +0900

    🔥 delete :: unused code

--- ToGather/app/src/main/java/com/tmdhoon/togather/view/fragment/ChatFragment.kt
+++ ToGather/app/src/main/java/com/tmdhoon/togather/view/fragment/ChatFragment.kt
@@ -1,36 +1,18 @@
 package com.tmdhoon.togather.view.fragment
 
 import android.os.Bundle
-import android.os.Handler
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.Fragment
 import com.tmdhoon.togather.R
 import com.tmdhoon.togather.databinding.FragmentChatBinding
-import com.tmdhoon.togather.dto.request.ChatRequest
-import com.tmdhoon.togather.dto.request.JoinRoomRequest
-import com.tmdhoon.togather.dto.response.ChatResponse
-import com.tmdhoon.togather.network.SocketProvider
-import com.tmdhoon.togather.util.ACCESS_TOKEN
-import com.tmdhoon.togather.util.printToast
-import com.tmdhoon.togather.viewmodel.ChatViewModel
-import io.socket.client.Manager
-import io.socket.client.Socket
-import io.socket.engineio.client.Transport
-import org.json.JSONArray
 
 class ChatFragment : Fragment() {
 
     private lateinit var binding: FragmentChatBinding
 
-    private val chatViewModel by lazy {
-        ViewModelProvider(this).get(ChatViewModel::class.java)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,16 +20,7 @@ class ChatFragment : Fragment() {
     ): View {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_chat, container, false)
-
-        chatViewModel.connectSocket()
-
         binding.lifecycleOwner = this
         return binding.root
-    }
-
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        chatViewModel.disconnectSocket()
     }
 }
