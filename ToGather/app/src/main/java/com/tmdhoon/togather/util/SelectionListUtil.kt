a4b667d4c849e773d6e3f9bd1b9c97d80f6da342
Tmdhoon2 <hks026naver@gmail.com>
2022-12-09 10:14:56 +0900

    🌱 add :: SelectionListUtil

---
+++ ToGather/app/src/main/java/com/tmdhoon/togather/util/SelectionListUtil.kt
@@ -0,0 +1,25 @@
+package com.tmdhoon.togather.util
+
+val arrayList = arrayListOf<String>()
+val selectedList = arrayListOf(
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+)
+
+val positionNameList = arrayListOf(
+    "ANDROID",
+    "iOS",
+    "PM",
+    "IOS",
+    "FRONTEND",
+    "DESIGNER",
+    "BACKEND",
+)
